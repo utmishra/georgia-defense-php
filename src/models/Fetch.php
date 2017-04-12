21fcc03bd266cfd26c85cd681a33d517e392d431
Utkarsh Mishra <utmishra@gmail.com>
2017-04-13 03:18:11 +0530

    Modified autocomplete to search from the starting characters only

--- src/models/Fetch.php
+++ src/models/Fetch.php
@@ -9,7 +9,7 @@ class Fetch {
 		$pdo = \GeorgiaPHP\DB::get();
 		$stmt = $pdo->prepare("SELECT companyName, duns FROM companies WHERE lower(companyName) LIKE ?");
 		$stmt->execute(
-			array("%$query%")
+			array("$query%")
 			) or die(print_r($stmt->errorInfo(), true));
 		$result = $stmt->fetchAll();
 		return $result;
