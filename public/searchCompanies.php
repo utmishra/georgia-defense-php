538555559d5f8907256f47cee799214ea0dd84f8
Utkarsh Mishra <utmishra@gmail.com>
2017-04-13 02:02:25 +0530

    Added public data retrieval API

---
+++ public/searchCompanies.php
@@ -0,0 +1,19 @@
+<?php
+
+header('Content-Type:application/json');
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use GeorgiaPHP\Models\Fetch;
+
+
+$query = $_GET['query'];
+$fetch = new GeorgiaPHP\Models\Fetch();
+$searchResponse = $fetch->searchByCompanyName($query);
+$finalResult = array();
+foreach ($searchResponse as $key => $value) {
+	$finalResult[$key]['duns'] = $value['duns'];
+	$finalResult[$key]['companyname'] = $value['companyname'];
+}
+echo json_encode($finalResult);
+?>
