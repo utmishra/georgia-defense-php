2429c284e60022f7e203011837947930b2de485a
Utkarsh Mishra <utmishra@gmail.com>
2017-07-14 00:33:25 +0530

    Testing less headers

--- public/sendMail.php
+++ public/sendMail.php
@@ -1,8 +1,8 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Credentials: true');
-header('Access-Control-Allow-Headers "origin, x-requested-with, content-type"');
-header('Access-Control-Allow-Methods "PUT, GET, POST, DELETE, OPTIONS"');
+// header('Access-Control-Allow-Credentials: true');
+// header('Access-Control-Allow-Headers "origin, x-requested-with, content-type"');
+// header('Access-Control-Allow-Methods "PUT, GET, POST, DELETE, OPTIONS"');
 // using SendGrid's PHP Library
 // https://github.com/sendgrid/sendgrid-php
 // If you are using Composer (recommended)
